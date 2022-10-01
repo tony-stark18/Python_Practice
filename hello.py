0674f560a0ffd3fcbd6518f7b51c8d54b4c56984
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2022-10-01 10:41:04 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+# a=input("Enter a number ")
+# b=input("Enter another number ")
+# print("Addition of ",a,"and ",b,"is ",int(a)+int(b))
+# a=input("Enter a number ")
+# print("Remainder when ",a,"is devided by 2 is ",int(a)%2)
+# int=10.0;
+# print(type(int))
+x="My"
+y='name'
+z='is Ak'
+print(x+' ' +y+' '+ z)
