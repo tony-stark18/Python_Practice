9ba00d51592585c897f4f313b77206be4db76d2e
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2022-10-03 00:01:41 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-# a=input("Enter a number ")
-# b=input("Enter another number ")
-# print("Addition of ",a,"and ",b,"is ",int(a)+int(b))
-# a=input("Enter a number ")
-# print("Remainder when ",a,"is devided by 2 is ",int(a)%2)
-# int=10.0;
-# print(type(int))
